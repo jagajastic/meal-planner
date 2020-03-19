d219b0db2172023546ecb6ecc4a22ccc326309e6
jagajastic <josephibrahi@gmail.com>
2020-03-19 13:35:05 +0100

    tie app with redux and make an event that fires add functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { addRecipe } from "./actions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    calendar: null
+  };
+
+  componentDidMount() {
+    const { store } = this.props;
+
+    store.subscribe(() => {
+      this.setState(() => ({
+        calendar: store.getState()
+      }));
+    });
+
+    this.input.value = "";
+  }
+
+  submitFood = () => {
+    this.props.store.dispatch(
+      addRecipe({
+        day: "monday",
+        meal: "breakfast",
+        recipe: { label: this.input.value }
+      })
+    );
+    console.log(this.input.value);
+    this.input.value = "";
+  };
+  render() {
+    console.log(this.state.calendar);
+    return (
+      <div>
+        <input
+          type="text"
+          ref={input => (this.input = input)}
+          placeholder="Monday's Breakfast"
+        />
+        <button onClick={this.submitFood}>Submit</button>
+
+        <pre>
+          Monady's Breakfast:{" "}
+          {this.state.calendar && this.state.calendar.monday.breakfast}
+        </pre>
+      </div>
+    );
+  }
 }
 
 export default App;
